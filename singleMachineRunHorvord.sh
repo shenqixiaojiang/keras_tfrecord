9eb9098eeed1463a04da8653abd96ce98f4fffec
shenqixiaojiang <shenshiwei11@163.com>
2018-04-23 15:32:20 +0800

    run the keras_tfrecord with horvord

---
+++ singleMachineRunHorvord.sh
@@ -0,0 +1,6 @@
+mpirun -np 3 \
+    -H localhost:3 \
+    -bind-to none -map-by slot \
+    -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH \
+    -mca pml ob1 -mca btl ^openib \
+    python mnist_horvord.py 
